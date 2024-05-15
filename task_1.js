0aacb63eb72461be12f5d11139acac2cc67fefdb
Yuliia Hryhorenko <yuliiahryhorenko@gmail.com>
2024-05-15 20:43:44 +0300

    fixing hw

--- task_1.js
+++ task_1.js
@@ -4,7 +4,6 @@ let values = [10, 20, 4, 6, 11, 9, 125, 1, 8, 0, 3];
 if (values.length > 0) {
     let minValue = Math.min(...values);
     let maxValue = Math.max(...values);
-
     console.log("Мінімальне значення - " + minValue);
     console.log("Максимальне значення - " + maxValue);
 } else {
